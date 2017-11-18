b6f2263301330a31f37eb20044531b3e8a8d047a
dohertki <kierind@gmail.com>
2017-11-18 14:12:33 -0800

    add useraccount table

--- sql_scripts/useraccounts.sql
+++ sql_scripts/useraccounts.sql
@@ -4,18 +4,18 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' ;
 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 ;
 
 
+CREATE TABLE useraccount(id INT PRIMARY KEY AUTO_INCREMENT, first_name VARCHAR(255) NOT NULL, last_name VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, zipcode INT, date DATE, message BOOLEAN);
 
 
 CREATE TABLE IF NOT EXISTS useraccount (
-	user_id int(11) NOT NULL AUTO_INCREMENT,
+	uid INT PRIMARY KEY AUTO_INCREMENT,
 	username varchar(255) NOT NULL,
 	email varchar(255) NOT NULL,
 	password varchar(32) NOT NULL,
 	zipcode int(32) NOT NULL,
     party varchar(32) NOT NULL,
-    reminder boolean(1) NOT NULL,
-    newsletter boolean(1) NOT NULL,
+    reminder boolean NOT NULL,
+    newsletter boolean NOT NULL,
 
-	PRIMARY KEY (user_id),
 	CONSTRAINT unique_constraint UNIQUE (username)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
