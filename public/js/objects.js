7a6d26d62bfa0d942c0e60c905f6d362cc126108
Peter Yetti <pyetti@users.noreply.github.com>
2017-11-19 12:06:40 -0500

    UI styling for election detail

--- public/js/objects.js
+++ public/js/objects.js
@@ -5,20 +5,20 @@ function messageAgent(firstName, lastName, email){
     this.firstName = firstName;
     this.lastName = lastName;
     this.email = email;
-    this.blankHtml =  `<!DOCTYPE html>
+    this.blankHtml =  <!DOCTYPE html>
         <html>
           <head>
           </head>
           <body>
             <div id="msgData"</div>
-          
+
           </body>
         </html>`;
 
     this.packageMessage = function(){
 
     };
-}    
+}
 
 
 
